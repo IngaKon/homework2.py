6471813f43dbbb41efb1fc5be29543234e2e7a98
IngaKon <kononova.inga@list.ru>
2024-06-30 08:05:22 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework=12
+hoursSpent=1.5
+name='Python'
+timeOneTask=hoursSpent/homework
+print('Курс:',name,',всего задач:',homework,", затраченно часов:",hoursSpent,', среднее время выполнения:',timeOneTask,)
